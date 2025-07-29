e067d862536c5b06a6d8427e6fb80903a90b9de3
stborr <st.borissova@abv.bg>
2025-07-29 19:25:31 +0300

    Delete src directory

--- src/first.java
+++
@@ -1,8 +0,0 @@
-public class first {
-    public static void main(String[] args) {
-        String firstName = "Stefani";
-        String middleName = "Valentinova";
-        String lastName = "Borisova";
-        System.out.println(firstName+" "+middleName+" "+ lastName);
-        }
-    }
